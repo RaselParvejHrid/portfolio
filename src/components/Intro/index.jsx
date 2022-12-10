a4b7915ffd65d8f247aa144382901ee04ceaac7e
Rasel Parvej <RaselParvejABC@gmail.com>
2022-12-10 23:14:00 +0600

    Intro Background Image Changed

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <section
       id="intro"
-      className="bg-[url('/introback.svg')] bg-center-bottom bg-fixed bg-no-repeat bg-cover h-[500px] md:h-[600px]  flex gap-0 md:gap-4 flex-col-reverse md:flex-row justify-around items-center"
+      className="bg-[url('/introback.jpg')] bg-center-bottom bg-fixed bg-no-repeat bg-cover h-[500px] md:h-[600px]  flex gap-0 md:gap-4 flex-col-reverse md:flex-row justify-around items-center"
     >
       <div>
         <h1 className="font-bold text-h5 md:text-h4 text-right">
@@ -27,7 +27,7 @@ const Intro = () => {
       </div>
 
       <div>
-        <img src="/me.png" className="w-64" />
+        <img src="/me.png" className="w-48" />
       </div>
     </section>
   );
