f0dfd425ca4038fe9d1a0acc0bf15f33f8db6df9
Rasel Parvej <RaselParvejABC@gmail.com>
2022-12-11 12:25:49 +0600

    Skills and About Updated

--- src/routes/Portfolio/components/Skills/index.jsx
+++ src/routes/Portfolio/components/Skills/index.jsx
@@ -8,30 +8,59 @@ const Skills = () => {
       </h1>
       <div className="grid gap-16 md:grid-cols-2 lg:grid-cols-3">
         <div className="shadow-lg p-8 rounded-md">
-          <h2 className="text-center font-semibold text-forest-green text-h6">
+          <h2 className="text-center font-bold m-3 text-pantone-green text-h6">
             Expert
           </h2>
-          <p>
-            HTML5, CSS3, Javascript (ES6), Bootstrap 5, React, React Router DOM,
-            Node, Express, Dart, Flutter, Data Structures, Algorithms
+          <p className="flex flex-wrap justify-center">
+            {`HTML5, CSS3, Javascript (ES6), Bootstrap 5, React, React Router DOM,
+            Node, Express, Dart, Flutter, Data Structures, Algorithms`
+              .split(",")
+              .map((tech, index) => (
+                <span
+                  key={index}
+                  className="border-rich-black-fogra border-2 text-body-small mx-1 my-1  px-2 py-0 rounded-md"
+                >
+                  {tech.trim()}
+                </span>
+              ))}
           </p>
         </div>
         <div className="shadow-lg p-8 rounded-md">
-          <h2 className="text-center font-semibold text-forest-green text-h6">
+          <h2 className="text-center font-bold m-3 text-pantone-green text-h6">
             Comfortable
           </h2>
-          <p>
-            Tailwind, Material Tailwind, DaisyUI, React DaisyUI, React
+          <p className="flex flex-wrap justify-center">
+            {`Tailwind, Material Tailwind, DaisyUI, React DaisyUI, React
             Bootstrap, Material UI, MongoDB, Firebase, React Query, Typescript,
             Git, GitHub, VSCode, Firebase Admin, C, C++, PHP, Java, Python,
-            Kotlin
+            Kotlin`
+              .split(",")
+              .map((tech, index) => (
+                <span
+                  key={index}
+                  className="border-rich-black-fogra border-2 text-body-small mx-1 my-1  px-2 py-0 rounded-md"
+                >
+                  {tech.trim()}
+                </span>
+              ))}
           </p>
         </div>
         <div className="shadow-lg p-8 rounded-md">
-          <h2 className="text-center font-semibold text-forest-green text-h6">
+          <h2 className="text-center font-bold m-3 text-pantone-green text-h6">
             Familiar
           </h2>
-          <p>Redux, React Redux, Redux Toolkit, NextJS, React Native</p>
+          <p className="flex flex-wrap justify-center">
+            {`Redux, React Redux, Redux Toolkit, NextJS, React Native`
+              .split(",")
+              .map((tech, index) => (
+                <span
+                  key={index}
+                  className="border-rich-black-fogra border-2 text-body-small mx-1 my-1  px-2 py-0 rounded-md"
+                >
+                  {tech.trim()}
+                </span>
+              ))}
+          </p>
         </div>
       </div>
     </section>
